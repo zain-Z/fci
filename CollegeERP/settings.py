a5c76b43b5e5cd435a79ecbab20409ea92c59ec8
unknown <zai1999n@gmail.com>
2021-06-02 00:30:56 +0200

    hello

--- CollegeERP/settings.py
+++ CollegeERP/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'jy8c-n9y=pf##!2^jae-l_5iafq6q%wfq8gdb6c0r5d52su+9y'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*', 'https://fci-scu.herokuapp.com/', '.herokuapp.com']
 
 AUTH_USER_MODEL = 'info.User'
 
